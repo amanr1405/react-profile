232129af8d8ee4a3b3d502b72c5ec4dfb9c69cc3
Aman <81345472+amanr1405@users.noreply.github.com>
2025-11-28 20:56:16 +0530

    changed the content

--- src/components/heroic.jsx
+++ src/components/heroic.jsx
@@ -5,7 +5,7 @@ function Heroic(){
     return(
         <>
         <div className="heroic">
-            <h1>SOFTWARE <span>ENGINEER</span></h1>
+            <h1>HELLO, I'M <span>AMAN RAJ</span></h1>
             <p>Passionate about creating intuitive and engaging user experiences. Specialize in transforming ideas into beautifully crafted products.</p>
         </div>
         </>
